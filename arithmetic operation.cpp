6088cfea91cbd4255f58ee4063af6a5857fcb02f
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-16 23:59:30 +0530

    arithmetic operation.cpp

---
+++ arithmetic operation.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=20;
+    int b=10;
+    int c;
+    c=a+b;
+    cout<<"THE ADDITION VALUE OF c:"<<c<<endl;
+    
+    c=a*b;
+    cout<<"THE MULTIPLICATION VALUE OF c:"<<c<<endl;
+   
+    c=a/b;
+    cout<<"THE DIVISION VALUE OF c:"<<c<<endl;
+   
+    c=a-b;
+    cout<<"THE SUBSTRACTION VALUE OF c:"<<c<<endl;
+   
+    return 0;
+}
+                       
+                       
