381e25dba24be5deb54f1dcda55d1b77e9e33568
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-16 23:53:49 +0530

    ADDITION.cpp

---
+++ ADDITION.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int b=20;
+    int sum;
+    cout<<"THE VALUE OF a: ";
+    cin>>a;
+    cout<<"THE VALUE OF b: ";
+    cin>>b;
+    sum=a+b;
+    cout<<"THE VALUE OF SUM: "<<sum;
+    return 0;
+}
