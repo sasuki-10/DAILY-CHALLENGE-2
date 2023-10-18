cc31980ea4aa851121d646ee8b2cec0ab744a048
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-18 08:17:54 +0530

    sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int b=20;
+    int sum;
+    cout<<"THE VALUE OF a:"<<a;
+    
+    cout<<"\nTHE VALUE OF b:"<<b;
+   
+    sum=a+b;
+    
+    cout<<"\nTHE VALUE OF SUM: "<<sum;
+   
+   
+    return 0;
+}
