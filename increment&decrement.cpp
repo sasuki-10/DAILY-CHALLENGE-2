37adc878c4d4c2a271901ec86fbbf8fa3f7301d8
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-18 08:20:29 +0530

    increment&decrement.cpp

---
+++ increment&decrement.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10;
+	
+	// preincrement.......
+	
+	int b=++a;
+	cout<<"value of a: "<<b;
+	
+	// predecrement.......
+	
+    int c=--a;
+	cout<<"\nvalue of a: "<<c;
+	
+	// postincrement.......
+	
+	int d=a++;
+	cout<<"\nvalue of a: "<<d;
+	
+	// postdecrement.......
+	
+	int e=a--;
+	cout<<"\nvalue of a: "<<e;
+}	
