6f1fbca22de958659d6112cd4587cdd545fcd93f
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-19 20:21:21 +0530

    if-else(2).cpp

---
+++ if-else(2).cpp
@@ -0,0 +1,42 @@
+//checking if-else condition..
+#include<iostream>
+using namespace std;
+int main()
+{
+ int num;
+ cout<<"ENTER YOUR NUMBER: ";
+ cin>>num;
+ if(num==1)
+ {
+     cout<<"sunday";
+ }
+ else if(num==2)
+ {
+     cout<<" monday";
+ }
+ else if(num==3)
+ {
+     cout<<" tuesday";
+ }
+ else if(num==4)
+ {
+     cout<<" wednesday";
+ }
+ else if(num==5)
+ {
+     cout<<" thursday";
+ }
+ else if(num==6)
+ {
+     cout<<" friday";
+ }
+ else if(num==7)
+ {
+     cout<<" saturday";
+ }
+ else
+ {
+     cout<<" not in week";
+ }
+ return 0;
+}
