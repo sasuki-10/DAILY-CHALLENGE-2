d42011794e301806e21c832f24f7d24e3838d539
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-19 23:21:03 +0530

    switch(1).cpp

---
+++ switch(1).cpp
@@ -0,0 +1,61 @@
+//checking switch statement..
+#include<iostream>
+using namespace std;
+int main()
+{
+ int num;
+ cout<<"ENTER YOUR NUMBER: ";
+ cin>>num;
+   switch(num)
+  {
+     case 1: (num==1);
+     
+     cout<<"sunday";
+      
+     break;
+    
+     case 2: (num==2);
+ 
+     cout<<"monday";
+     
+     break;
+ 
+     case 3: (num==3);
+ 
+     cout<<"tuesday";
+     
+     break;
+ 
+     case 4: (num==4);
+ 
+     cout<<"wednesday";
+     
+     break;
+ 
+     case 5: (num==5);
+ 
+     cout<<"thursday";
+     
+     break;
+ 
+     case 6: (num==6);
+ 
+     cout<<"friday";
+     
+     break;
+ 
+     case 7: (num==7);
+ 
+     cout<<"saturday";
+     
+     break;
+ 
+     default:
+ 
+     cout<<" not in week";
+     
+    }
+ 
+ return 0;
+ 
+}
