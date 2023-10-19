9b0e4edf367a8841c9d4e4429eb22d4e6207c3aa
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-19 20:20:18 +0530

    if-else(1).cpp

---
+++ if-else(1).cpp
@@ -0,0 +1,19 @@
+//checking if-else condition..
+#include<iostream>
+using namespace std;
+int main()
+{
+ int age;
+ cout<<"ENTER YOUR AGE TO CHECK WHETHER YOU ARE ELLIGIBLE TO VOTE OR NOT: ";
+ cin>>age;
+ if(age>=18)
+ {
+     cout<<"***********ELLIGIBLE TO VOTE***************";
+ }
+ else
+ {
+     cout<<"***********NOT ELLIGIBLE TO VOTE****************";
+ }
+ 
+ return 0;
+}
