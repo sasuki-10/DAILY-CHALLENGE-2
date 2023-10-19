ee90013089f8f6e78a77b91bb99abf3a7bc5606e
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-19 23:39:17 +0530

    pattern(loop).cpp

---
+++ pattern(loop).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int num,fact=1;
+  cout<<"ENTER THE NUMBER:";
+  cin>>num;
+   
+  for(int i=1;i<=num;i++)
+    {
+      fact=fact*i;
+    } 
+    cout<<"FACTORIAL OF THE NUMBER: "<<fact;   
+} 
+
+    
