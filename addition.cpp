20349f193a72bd9f57d924a98d3a7ffcdb99de09
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-24 23:56:15 +0530

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class addition
+{
+    public:
+    int sum,a,b;
+    int set_sum(int a,int b)
+   {
+    return (a+b);
+   }
+};
+    int main()
+{
+    addition tr;
+    tr.set_sum(10,2);
+    cout<<tr.sum<<endl;
+    
+}
