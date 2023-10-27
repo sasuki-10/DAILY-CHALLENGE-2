fe317d2e1a89d34479699b339611fcc60f596fd6
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-27 23:18:15 +0530

    Create 3.Decreasing patter.cpp

---
+++ 3.Decreasing patter.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j;
+	
+	for(i=5;i>=0;i--)              //It shows upto how many rows pattern has to be updated
+	{                                            
+		for(j=1;j<=i;j++)             //It shows column.           
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
