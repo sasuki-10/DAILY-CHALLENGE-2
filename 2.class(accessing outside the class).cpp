e17bcbde42dac6cc2cfd6e0ca6f2410b04a61324
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-28 22:43:46 +0530

    Create 2.class(accessing outside the class).cpp

---
+++ 2.class(accessing outside the class).cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class profile
+{
+  public:
+ 
+  string name;
+   int id;	
+  
+  void printname();
+  
+  void print_name()
+  {
+  	cout<<"name is:"<<name;
+  }
+};
+
+void profile::printname()
+{
+	cout<<"Id is:"  <<id;
+}
+int main()
+{ 	 
+profile tr;
+tr.name="tripti";
+tr.id=24;
+
+tr.printname();
+cout<<endl;
+tr.print_name();
+return 0;
+
+}
+
+
+
+
