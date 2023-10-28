b1c77509482922c30c0742b5772ff14e35a28be9
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-28 21:29:37 +0530

    Create portfolio.cpp

---
+++ portfolio.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class profile
+{
+  public:
+  int age;
+  int roll_number;
+  string name;	
+};
+int main()
+{ 	
+profile tr;
+tr.name="tripti";
+tr.roll_number=024616;
+tr.age=19;
+cout<<"NAME:"<<tr.name<<endl;
+
+cout<<"roll number:"<<tr.roll_number<<endl;
+
+cout<<"AGE"<<tr.age;
+
+}
+
+
+
+
