027f6dea43c581b25349d157a79d6b4972f61be4
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-01 22:37:01 +0530

    constructor(1).cpp

---
+++ constructor(1).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class A
+{
+	public:
+		A()                   //same name of class  {default constructor}
+		{
+		   cout<<"C++";// std::cout if "using namespace" not written
+		}
+};
+int main()
+{
+	A obj;                  //object invoked
+}
