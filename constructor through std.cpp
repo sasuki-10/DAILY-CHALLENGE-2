3d48352f8be4b55934da7e9f593d70af16e4fc9f
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-01 22:40:47 +0530

    constructor through std.cpp

---
+++ constructor through std.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+class A
+{
+	public:
+		A()                   //same name of class  {default constructor}
+		{
+		   std::cout<<"C++";// std::cout if "using namespace" not written
+		}
+};
+int main()
+{
+	A obj;                  //object invoked
+}
