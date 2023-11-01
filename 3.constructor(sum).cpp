7abdb467eb3c28abf881ae63ff3b64fc2346247f
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-01 22:45:40 +0530

    3.constructor(sum).cpp

---
+++ 3.constructor(sum).cpp
@@ -0,0 +1,19 @@
+include<iostream>
+using namespace std;
+class sum
+{
+	public:
+		sum(int a,int b)
+		{
+			int c;
+			c=a+b;
+			cout<<"sum is:"<<c;
+			
+		}
+		
+};
+int main()
+{
+	sum obj(20,30);
+	
+}
