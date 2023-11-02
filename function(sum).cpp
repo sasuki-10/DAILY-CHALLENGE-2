7c3593fff16860414cad206335a156b5f1d94ac6
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-02 23:30:48 +0530

    Create function(sum).cpp

---
+++ function(sum).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int sum(int a,int b)
+{
+	int c;
+	c=a+b;
+	cout<<"SUM IS:"<<c;
+}
+int main()
+{
+	int a,b;
+	cout<<"ENTER THE VALUE OF A:";
+	cin>>a;
+	cout<<"ENTER THE VALUE OF B:";
+	cin>>b;
+    sum(a,b);
+ return 0;		
+}
+
+
+
+
+
+
+
+
