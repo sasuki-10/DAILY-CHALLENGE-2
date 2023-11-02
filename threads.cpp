e36487ba7c68385e0cd45ce86b9fa27630728df6
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-02 23:33:13 +0530

    Create threads.cpp

---
+++ threads.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<thread>
+using namespace std;
+int threadfn()
+{
+	cout<<"My thread";
+}
+int main()
+{
+ thread t1(threadfn);
+ t1.join();   //synchronising process
+ return 0;
+}
