b76791a7c16a7c23417947361fd527126b3ed5e6
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-08 22:06:30 +0530

    Decreasing patter-3.cpp

--- Decreasing patter-3.cpp
+++ Decreasing patter-3.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int main()
 {
 	int i,j;
-	
 	for(i=5;i>=0;i--)              //It shows upto how many rows pattern has to be updated
 	{                                            
 		for(j=1;j<=i;j++)             //It shows column.           
