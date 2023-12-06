9d8a6806873567ee6c2516c9770f88ee327745cc
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-12-06 23:54:12 +0530

    array copy.cpp

--- array copy.cpp
+++ array copy.cpp
@@ -5,14 +5,16 @@ int main()
  int arr1[5];
  int arr2[5];
  int i;
- cout<<"array elements are:";
-    for(i=0;i<5;i++)
+
+cout<<"array elements are:";
+    
+	for(i=0;i<5;i++)
       {
  	        cin>>arr1[i];
  		arr2[i]=arr1[i];
       }	
 
-	cout<<"\n/*******DISPLAYING***********/";
+cout<<"\n/*******DISPLAYING***********/";
 	
 	for(i=0;i<5;i++)
        {
